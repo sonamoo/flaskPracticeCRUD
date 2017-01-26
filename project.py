c3ff0ec6faf84e4556acc292ee4d3e3406e8dc74
Seho Lim <limseho657424@gmail.com>
2017-01-25 22:09:46 -0600

    added json api for a specific menu

--- project.py
+++ project.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -9,13 +11,19 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Making an API Endpoint (GET Request)
 @app.route('/restaurants/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id=
-                                              restaurant_id).all()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
     return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def menuJSON(restaurant_id, menu_id):
+    menu = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=menu.serialize)
 
 
 @app.route('/')
